bdde64ceceebcde5f311b5596036a9ced1df1628
Umberto Sonnino <rimher@gmail.com>
2016-12-03 15:29:04 +0100

    Initial Commit

---
+++ SDL2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL2
+//
+//  Created by Umberto Sonnino on 3/12/16.
+//  Copyright © 2016 Umberto Sonnino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
